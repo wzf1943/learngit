25a1c73ab55d393d0d47168b62b1fb15ce367b20
Joseph Wong <wzf2012@u.northwestern.edu>
2017-09-24 15:54:41 -0700

    Add CC 150

---
+++ CC150/1.6/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string compressStr (string str) {
+    int count = 0;
+    int length = str.length();
+    int i = 1, j = 0;
+    
+    string res = "";
+
+    for (; i < length; ) {
+        if (str[j] != str[i]) {
+            count = i - j;
+            res.push_back(str[j]);
+            res += to_string(count);
+            j = i;
+            i++;
+            count = 0;
+        } else {
+            i++;
+        }
+    }
+
+    if (res.length() >= str.length()) {
+        return str;
+    }
+    return res;
+}
+
+int main() {
+    string str = "aabcccccaaa";
+    string out = compressStr(str);
+    cout << out << endl;
+    return 0;
+}
+
+
